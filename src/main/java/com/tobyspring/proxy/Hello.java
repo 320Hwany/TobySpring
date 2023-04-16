bdb72169f959c538342f1d176d8f8e229ac4832d
Jung-YouHwan <yhwjd99@gmail.com>
2023-04-16 14:53:23 +0900

    프록시 패턴

---
+++ src/main/java/com/tobyspring/proxy/Hello.java
@@ -0,0 +1,10 @@
+package com.tobyspring.proxy;
+
+public interface Hello {
+
+    String sayHello(String name);
+
+    String sayHi(String name);
+
+    String sayThankYou(String name);
+}
---
+++ src/test/java/com/tobyspring/proxy/ProxyTest.java
@@ -0,0 +1,21 @@
+package com.tobyspring.proxy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ProxyTest {
+
+    @Test
+    void simpleProxy() {
+        HelloTarget hello = new HelloTarget();
+        assertThat(hello.sayHello("Toby")).isEqualTo("Hello Toby");
+        assertThat(hello.sayHi("Toby")).isEqualTo("Hi Toby");
+        assertThat(hello.sayThankYou("Toby")).isEqualTo("Thank You Toby");
+
+        Hello proxiedHello = new HelloUppercase(new HelloTarget());
+        assertThat(proxiedHello.sayHello("Toby")).isEqualTo("HELLO TOBY");
+        assertThat(proxiedHello.sayHi("Toby")).isEqualTo("HI TOBY");
+        assertThat(proxiedHello.sayThankYou("Toby")).isEqualTo("THANK YOU TOBY");
+    }
+}
